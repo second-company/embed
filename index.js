71c89ee6b9d1677427dfc49ee9348bb5f058b950
Wolfie <45970663+wolfiediscord@users.noreply.github.com>
2020-04-13 15:55:58 -0500

    Update index.js
    Changing custom emotes to unicode ones.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.success = (msg) => {
 if(!msg) throw '[ERR] You need to have a valid message on the function "embed".'
     let embed = {
-     description: `<:wolfcheckmark:695361282219442286> ${msg}`,
+     description: `✅ ${msg}`,
      color: '#2fa811'
    }
    return embed;
@@ -9,7 +9,7 @@ if(!msg) throw '[ERR] You need to have a valid message on the function "embed".'
 exports.warn = (msg) => {
 if(!msg) throw '[ERR] You need to have a valid message on the function "embed".'
     let embed = {
-     description: `<:wolfwarn:697276847422963733> ${msg}`,
+     description: `⚠️ ${msg}`,
      color: '#fced1c'
     }
     return embed;
@@ -17,7 +17,7 @@ if(!msg) throw '[ERR] You need to have a valid message on the function "embed".'
 exports.info = (msg) => {
 if(!msg) throw '[ERR] You need to have a valid message on the function "embed".'
     let embed = {
-    description: `<:wolfinfo:697279215174680589>  ${msg}`,
+    description: `ℹ️ ${msg}`,
     color: '#3c8de7'
     }
     return embed;
@@ -25,7 +25,7 @@ if(!msg) throw '[ERR] You need to have a valid message on the function "embed".'
 exports.error = (msg) => {
     if(!msg) {this.msg = 'Unknown. Please contact the bot author.'};
     let embed = {
-      description: `<:wolfx:695361329803821086> ${msg}`,
+      description: `❌ ${msg}`,
       color: '#ff0000'
     }
     return embed;
